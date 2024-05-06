dadb4d3eea31783192760f7804584611a64a6556
ElenaKanash <hird250@gmail.com>
2024-05-06 18:20:07 +0300

    add timeout

---
+++ tests/mainPage.spec.js
@@ -0,0 +1,13 @@
+import { test, expect} from "@playwright/test";
+
+test.describe('Main page', () => {
+  test('Check one element', async({page}) => {
+    await page.goto('https://demoqa.com/');
+
+    //await page.locator('.card:nth-child(2)').click();
+    await page.getByText('Forms').click();
+
+    await expect(page).toHaveURL('https://demoqa.com/forms')
+  });
+  
+});
