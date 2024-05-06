4c6d23eebca1151c70458dfd60ab65497d930f53
ElenaKanash <hird250@gmail.com>
2024-05-06 18:21:53 +0300

    add test 'Check one element'

--- tests/mainPage.spec.js
+++ tests/mainPage.spec.js
@@ -3,7 +3,6 @@ import { test, expect} from "@playwright/test";
 test.describe('Main page', () => {
   test('Check one element', async({page}) => {
     await page.goto('https://demoqa.com/');
-
     //await page.locator('.card:nth-child(2)').click();
     await page.getByText('Forms').click();
 
