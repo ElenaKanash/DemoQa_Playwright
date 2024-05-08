5971f2b43793b0473346e21d501f4544e585d309
ElenaKanash <hird250@gmail.com>
2024-05-08 19:42:54 +0300

    add test 'Validating the form with empty fields'

--- tests/practiceForm.spec.js
+++ tests/practiceForm.spec.js
@@ -2,7 +2,20 @@ import { test, expect } from "@playwright/test";
 import { beforeEach } from "node:test";
 
 test.describe('Practice Form', () => {
+  let firstName = 'Ivan';
+  let lastName = 'Ivanov';
+  let email = 'q1@gmail.com'
+  let firstNameField;
+  let lastNameField;
+  let mobileNumberField;
+  let emailField;
+
   test.beforeEach(async ({ page }) => {
+    firstNameField = page.getByPlaceholder('First Name');
+    lastNameField = page.getByRole('textbox', { name: 'Last Name' });
+    mobileNumberField = page.getByRole('textbox', 'Mobile Number');
+    emailField = page.getByPlaceholder('name@example.com');
+
     await page.goto('https://demoqa.com/automation-practice-form');
     /*   await page.goto('https://demoqa.com/');    
       await page.getByText('Forms').click();
@@ -10,17 +23,21 @@ test.describe('Practice Form', () => {
   })
 
   test('Filling Practice Form', async ({ page }) => {
-    const firstNameField = page.getByPlaceholder('First Name');    
-    const firstName = 'Ivan';
-    const lastNameField = page.getByRole('textbox', {name: 'Last Name'});
-    const lastName = 'Ivanov';
-
     await firstNameField.fill(firstName);
     await lastNameField.fill(lastName);
+    await emailField.fill(email)
 
-    await expect(firstNameField).toHaveValue(firstName);    
+    await expect(firstNameField).toHaveValue(firstName);
     await expect(lastNameField).toHaveValue(lastName);
+    await expect(emailField).toHaveValue(email);
+  });
 
+  test('Validating the form with empty fields', async ({ page }) => {
+    await page.getByRole('button', { name: 'Submit' }).click();
+
+    await expect(firstNameField).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+    await expect(lastNameField).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+    await expect(mobileNumberField).toHaveCSS('border-color', 'rgb(220, 53, 69)');
   });
 
 
