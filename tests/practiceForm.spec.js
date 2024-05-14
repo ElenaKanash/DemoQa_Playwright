d335396b0b2dc88e32028b4baa5e9eb20895735b
ElenaKanash <hird250@gmail.com>
2024-05-14 23:34:10 +0300

    add expects

--- tests/practiceForm.spec.js
+++ tests/practiceForm.spec.js
@@ -4,7 +4,8 @@ import { beforeEach } from "node:test";
 test.describe('Practice Form', () => {
   let firstName = 'Ivan';
   let lastName = 'Ivanov';
-  let email = 'q1@gmail.com'
+  let email = 'q1@gmail.com';
+  let mobileNumber = '1111111111';
   let firstNameField;
   let lastNameField;
   let mobileNumberField;
@@ -13,7 +14,7 @@ test.describe('Practice Form', () => {
   test.beforeEach(async ({ page }) => {
     firstNameField = page.getByPlaceholder('First Name');
     lastNameField = page.getByRole('textbox', { name: 'Last Name' });
-    mobileNumberField = page.getByRole('textbox', 'Mobile Number');
+    mobileNumberField = page.getByPlaceholder('Mobile Number');
     emailField = page.getByPlaceholder('name@example.com');
 
     await page.goto('https://demoqa.com/automation-practice-form');
@@ -24,6 +25,7 @@ test.describe('Practice Form', () => {
 
   test('Filling Practice Form', async ({ page }) => {
     const genreMale = page.locator('[for="gender-radio-1"]');
+    const hobbiesSport = page.locator('[for="hobbies-checkbox-1"]')
 
     await firstNameField.fill(firstName);
     await expect(firstNameField).toHaveValue(firstName);
@@ -33,14 +35,23 @@ test.describe('Practice Form', () => {
     await expect(emailField).toHaveValue(email);
     await genreMale.check()  
     await expect(genreMale).toBeChecked();
+    await mobileNumberField.fill(mobileNumber);
+    await expect(mobileNumberField).toHaveValue(mobileNumber);
+    await hobbiesSport.check(); 
+    await page.getByRole('button', { name: 'Submit' }).click();
+
+    await expect (page.locator('.modal-content')).toBeVisible();
+    await expect(page.locator('.h4')).toHaveText('Thanks for submitting the form');
+
   });
 
-  test('Validating the form with empty fields', async ({ page }) => {
+  test('Validating the form with empty fields', async ({ page }) => {    
     await page.getByRole('button', { name: 'Submit' }).click();
 
     await expect(firstNameField).toHaveCSS('border-color', 'rgb(220, 53, 69)');
     await expect(lastNameField).toHaveCSS('border-color', 'rgb(220, 53, 69)');
     await expect(mobileNumberField).toHaveCSS('border-color', 'rgb(220, 53, 69)');
+    await expect(page.locator('[for="gender-radio-1"]')).toHaveCSS('border-color', 'rgb(220, 53, 69)');
   });
 
 
