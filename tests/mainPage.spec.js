4d5d5329b00694a0268ff41945b314bed5c284e7
ElenaKanash <hird250@gmail.com>
2024-05-24 23:33:26 +0300

    add test('Check any element with tohaveText method')

--- tests/mainPage.spec.js
+++ tests/mainPage.spec.js
@@ -1,8 +1,11 @@
 import { test, expect } from "@playwright/test";
 
 test.describe('Main page', () => {
-  test('Check one element', async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto('https://demoqa.com/');
+  })
+
+  test('Check one element', async ({ page }) => {
     //await page.locator('.card:nth-child(2)').click();
     await page.getByText('Forms').click();
 
@@ -10,8 +13,6 @@ test.describe('Main page', () => {
   });
 
   test('Check any element', async ({ page }) => {
-    await page.goto('https://demoqa.com/');
-
     const expectedCards = ['Elements', 'Forms', 'Alerts, Frame & Windows', 'Widgets', 'Interactions', 'Book Store Application'];
     const cardElements = await page.locator('div.card').all();
 
@@ -21,6 +22,14 @@ test.describe('Main page', () => {
       const elementText = await cardElements[i].textContent();
       expect(elementText).toEqual(expectedCards[i]);
     }
+  });
+
+  test('Check any element with tohaveText method', async ({ page }) => {
+    const expectedCards = ['Elements', 'Forms', 'Alerts, Frame & Windows', 'Widgets', 'Interactions', 'Book Store Application'];
+    const cardElements = await page.locator('.category-cards .card');
+    //console.log(await cardElements.allInnerTexts());
+    await expect(cardElements).toHaveCount(6);
+    await expect(cardElements).toHaveText(expectedCards);
   });
 
 });
