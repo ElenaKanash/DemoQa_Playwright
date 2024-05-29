47eabbb8b12069440cabd8bd7fa6a7bccf42b9ff
ElenaKanash <hird250@gmail.com>
2024-05-30 01:17:07 +0300

    add test('Add some new Users in the table')

--- tests/webTable.spec.js
+++ tests/webTable.spec.js
@@ -13,7 +13,6 @@ test.describe('Web Table', () => {
 
 
   test('Сhange the number of rows in the table', async ({ page }) => {
-
     let tableRows = page.getByRole('rowgroup');
     await expect(tableRows).toHaveCount(10);
 
@@ -23,7 +22,6 @@ test.describe('Web Table', () => {
 
   test('Add new User in the row of the table', async ({ page }) => {
     await page.getByRole('button', { name: 'Add' }).click();
-
     await page.getByPlaceholder('First Name').fill('Ivan');
     await page.getByPlaceholder('Last Name').fill('Ivanov');
     await page.getByPlaceholder('name@example.com').fill('Ivan@example.com');
@@ -35,5 +33,30 @@ test.describe('Web Table', () => {
     await expect(page.getByRole('gridcell')).toContainText(['Ivan', 'Ivanov', '25',
       'Ivan@example.com', '1000', 'QA department']);
   });
+
+  test('Add some new Users in the table', async ({ page }) => {
+    const nextButton = page.getByRole('button', { name: 'Next' });
+    const previousButton = page.getByRole('button', { name: 'Previou' });
+
+    await page.getByLabel('rows per page').selectOption('5');
+    await expect(previousButton).toBeDisabled();
+    await expect(nextButton).toBeDisabled();
+
+    for (let i = 1; i < 5; i++) {
+      await page.getByRole('button', { name: 'Add' }).click();
+      await page.getByPlaceholder('First Name').fill('Ivan_' + i);
+      await page.getByPlaceholder('Last Name').fill('Ivanov_' + i);
+      await page.getByPlaceholder('name@example.com').fill('Ivan_' + i + '@example.com');
+      await page.getByPlaceholder('Age').fill('2' + i);
+      await page.getByPlaceholder('Salary').fill('100' + i);
+      await page.getByPlaceholder('Department').fill('QA department');
+      await page.getByRole('button', { name: 'Submit' }).click();
+    }
+
+    await expect(previousButton).toBeDisabled();
+    await expect(nextButton).toBeEnabled();
+  });
+
+
 
 });
