b3f5e2951c61bfc2e9e293bf905718cb6213cb04
ElenaKanash <hird250@gmail.com>
2024-05-29 23:46:59 +0300

    add test('Сhange the number of rows in the table')

--- tests/webTable.spec.js
+++ tests/webTable.spec.js
@@ -3,18 +3,24 @@ import { test, expect } from "@playwright/test";
 test.describe('Web Table', () => {
 
   test.beforeEach(async ({ page }) => {
-    /* await page.goto('https://demoqa.com/');
+    /* await page.goto('/');
     await page.getByText('Elements').click();
-    await page.getByText('Web Tables').click()
-
+    await page.getByText('Web Tables').click();
     await expect(page).toHaveURL('https://demoqa.com/webtables'); */
 
-    await page.goto('https://demoqa.com/webtables');
+    await page.goto('/' + 'webtables');
   })
 
 
-  test('Add one row to the table', async ({ page }) => {
+  test('Сhange the number of rows in the table', async ({ page }) => {
 
+    let tableRows =  page.getByRole('rowgroup');
+    await expect(tableRows).toHaveCount(10);
+
+    await page.getByLabel('rows per page').selectOption('5');
+    await expect(tableRows).toHaveCount(5);
   });
+
+  
 
 });
