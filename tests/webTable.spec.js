ea13b47e3fa420362f95dde063c90a5238c9c952
ElenaKanash <hird250@gmail.com>
2024-05-30 00:28:19 +0300

    add test('Add new User in the row of the table')

--- tests/webTable.spec.js
+++ tests/webTable.spec.js
@@ -14,13 +14,26 @@ test.describe('Web Table', () => {
 
   test('Сhange the number of rows in the table', async ({ page }) => {
 
-    let tableRows =  page.getByRole('rowgroup');
+    let tableRows = page.getByRole('rowgroup');
     await expect(tableRows).toHaveCount(10);
 
     await page.getByLabel('rows per page').selectOption('5');
     await expect(tableRows).toHaveCount(5);
   });
 
-  
+  test('Add new User in the row of the table', async ({ page }) => {
+    await page.getByRole('button', { name: 'Add' }).click();
+
+    await page.getByPlaceholder('First Name').fill('Ivan');
+    await page.getByPlaceholder('Last Name').fill('Ivanov');
+    await page.getByPlaceholder('name@example.com').fill('Ivan@example.com');
+    await page.getByPlaceholder('Age').fill('25');
+    await page.getByPlaceholder('Salary').fill('1000');
+    await page.getByPlaceholder('Department').fill('QA department');
+    await page.getByRole('button', { name: 'Submit' }).click();
+
+    await expect(page.getByRole('gridcell')).toContainText(['Ivan', 'Ivanov', '25',
+      'Ivan@example.com', '1000', 'QA department']);
+  });
 
 });
