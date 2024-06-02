09b8a2c5cd306ff7c45af0329e73ef872bdcab98
ElenaKanash <hird250@gmail.com>
2024-06-02 23:45:46 +0300

    refactor beforeEach

--- tests/webTable.spec.js
+++ tests/webTable.spec.js
@@ -3,12 +3,11 @@ import { test, expect } from "@playwright/test";
 test.describe('Web Table', () => {
 
   test.beforeEach(async ({ page }) => {
-    /* await page.goto('/');
+    //await page.goto('/' + 'webtables');
+     await page.goto('/');
     await page.getByText('Elements').click();
     await page.getByText('Web Tables').click();
-    await expect(page).toHaveURL('https://demoqa.com/webtables'); */
-
-    await page.goto('/' + 'webtables');
+    await expect(page).toHaveURL('/' + 'webtables');     
   })
 
 
